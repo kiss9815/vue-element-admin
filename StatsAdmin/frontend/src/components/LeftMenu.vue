fdaf31eafb78906fa22d553681929ded20800674
kiss9815 <kiss9815@gmail.com>
2019-05-21 14:22:30 +0900

    ㄴㅇㄹㄴㅇ
    ㄴㅇㄹㄴㅇㄹ

---
+++ StatsAdmin/frontend/src/components/LeftMenu.vue
@@ -0,0 +1,82 @@
+<template>
+
+<el-container style="border: 1px solid #eee">
+    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+        <el-submenu index="1">
+            <template slot="title">
+            <i class="el-icon-location"></i>
+            <span slot="title">Navigator One</span>
+            </template>
+            <el-menu-item-group>
+            <span slot="title">Group One</span>
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+            <span slot="title">item four</span>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Navigator Four</span>
+        </el-menu-item>
+    </el-menu>
+  
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>View</el-dropdown-item>
+          <el-dropdown-item>Add</el-dropdown-item>
+          <el-dropdown-item>Delete</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>Tom</span>
+    </el-header>
+    
+  </el-container>
+</el-container>
+</template>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      };
+    },
+     methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      isCollapse(){
+
+      }
+    }
+  };
+</script>
