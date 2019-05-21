9d0828676a72c33a76d1ac6c5535b60f3a78abda
kiss9815 <kiss9815@gmail.com>
2019-05-21 14:37:08 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(){
+		
+		log.info("index");
+		
+		return "index";
+	}
+}
