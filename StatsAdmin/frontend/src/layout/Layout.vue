16191db92b16a7f5ed50f3fe9e7a248cfb2f4d70
kiss9815 <kiss9815@gmail.com>
2019-05-21 14:29:00 +0900

    ㅇㅇ
    ㅇㅇ

---
+++ StatsAdmin/frontend/src/layout/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrap">
+        <LeftMenu></LeftMenu>
+    </div>
+    
+</template>
+
+<script>
+
+import LeftMenu from '@/views/LeftMenu';
+
+export default {
+  name: 'Layout',
+  components: { LeftMenu },
+  methods: {
+  },
+  computed: {
+    
+  },
+  created() {
+
+  }
+};
+</script>
+
+
